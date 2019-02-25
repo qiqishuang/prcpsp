63d23b7c2dc4207f8d468c015a07e45ecace6329
kermit-z <d.torressanchez@lancaster.ac.uk>
2019-02-25 12:25:13 +0000

    updated local constraint algorithm

--- lcalg.py
+++ lcalg.py
@@ -18,19 +18,15 @@ def get_constants(data):
     r = data['r']  # len(r) = 32
     E = range(n - 2)  # E = [0,...,29]
     V = range(1, n + 1)  # V = [1,...,32]
-    A = []
-    for (i, j) in [item for sublist in data['A'] for item in sublist]:
-        if i != 1 and j != n:
-            A.append((i, j))
-    LB = data['LB']
-    return n, T, K, p, R, r, E, V, A, LB
+    A = [item for sublist in data['A'] for item in sublist]
+    return n, T, K, p, R, r, E, V, A
 
 
-def load_data(it):
-    f = open('./input/j301_%s.sm' % it, "r")
+def load_data(i, j):
+    f = open("./input/j30/j30%s_%s.sm" % (i, j), "r")
     raw_lines = (f.read().splitlines())
     data = {}
-    data['A'], data['p'], data['R'], data['r'], data['LB'] = [], [], [], [], []
+    data['A'], data['p'], data['R'], data['r'] = [], [], [], []
     line_counter = 0
 
     for line in raw_lines:
@@ -39,7 +35,7 @@ def load_data(it):
         if len(integer_list) == 0:
             continue
         else:
-            if line_counter <= 13:
+            if line_counter <= 17:
                 if 'jobs' in line:
                     data['n'] = integer_list[0]
                 elif 'horizon' in line:
@@ -47,18 +43,15 @@ def load_data(it):
                 elif '- renewable' in line:
                     data['K'] = integer_list[0]
                 line_counter = line_counter + 1
-            elif line_counter == 15:
-                data['LB'] = integer_list[3]
-                # line_counter = line_counter + 1
             # PRECEDENCE RELATIONS - 49 > ... > 18
-            elif int(data['n']) + 16 >= line_counter >= 18:
+            elif int(data['n']) + 17 >= line_counter >= 18:
                 # integer_list[3:] = successors
                 if len(integer_list[3:]) >= 1:
-                    jobnr = integer_list[0]
+                    jobnr = integer_list[0]                  # jobnr.
                     data['A'].append([(jobnr, successor)
                                       for successor in integer_list[3:]])
             # 82 > ... > 50
-            elif 2 * int(data['n']) + 16 >= line_counter > int(data['n']) + 16:
+            elif 2 * int(data['n']) + 18 >= line_counter > int(data['n']) + 18:
                 data['p'].append(integer_list[2])
                 data['r'].append(integer_list[3:])
             line_counter = line_counter + 1
@@ -124,9 +117,9 @@ def algorithm(data):
              1 - p[j - 1] and (i, j) not in A]
         if len(E) != 0:
             A = list(set(E + A))
-            return A
+            return A, True
         else:
-            return A
+            return A, False
 
     def _symmetric_triples(A, b, D):
         F_3 = _get_F(3, A)
@@ -136,9 +129,9 @@ def algorithm(data):
             print(len(ST))
             for [i, j] in ST:
                 D.append([i, j])
-            return D
+            return D, True
         else:
-            return D
+            return D, False
 
     def _frequency(D):
         f = dict(collections.Counter(list(itertools.chain.from_iterable(D))))
@@ -185,8 +178,7 @@ def algorithm(data):
             namespace['N_%d' % i] = Node('%s' % i)
         for i in V:
             namespace['N_%d' % i].neighbors = \
-                [namespace['N_%d' % item[1]] for item in D
-                 if item[0] == i] +\
+                [namespace['N_%d' % item[1]] for item in D if item[0] == i] +\
                 [namespace['N_%d' % item[0]] for item in D if item[1] == i]
         return
 
@@ -209,35 +201,6 @@ def algorithm(data):
             subsets.update(set(itertools.combinations(C, m)))
         return subsets
 
-    def _edge_finding(b, C):
-        b_1_j, b_i_1, ind = {}, {}, 0
-        for j in C:
-            print(j)
-            C.remove(j)
-            print(j, C)
-            C_subsets = _subsets(C)
-            if min(b[1][i] for i in C) + sum(p[i - 1] for i in C) >\
-                    max(-b[i][1] + p[i - 1] for i in C if i != j):
-                print('True')
-                b[1][j] = max(b[1][j], max(
-                    (min(b[1][i] for i in C_1) + sum(p[i - 1] for i in C_1))
-                    for C_1 in C_subsets))
-                b_1_j['%s' % j] = b[1][j]
-                for i in C:
-                    if i != j:
-                        b[i][1] = max(b[i][1], b[j][1] + p[i - 1])
-                        b_i_1['%s' % i] = b[i][1]
-                    else:
-                        continue
-            else:
-                print('False')
-            if 0 <= ind <= len(C) - 1:
-                C.insert(ind, j)
-                ind += 1
-            else:
-                pass
-        return b_1_j, b_i_1
-
     def _get_last(clique, b):
         sub_cliques = _subsets(clique)
         # print(sub_cliques)
@@ -249,73 +212,74 @@ def algorithm(data):
             d['%s' % (str(Q))] = b[1][n]
         return d
 
-    n, T, K, p, R, r, E, V, A, LB = get_constants(data)
+    def _edge_finding():
+        cliques = list(itertools.chain.from_iterable(_get_clique(D)))
+        cliques_max = [item for item in cliques if len(
+            item) == max(len(item) for item in cliques)]
+        clique_max = []
+        for item in cliques_max:
+            item = [int(str(item[i])) for i in range(len(item))]
+            item.sort()
+            if item not in clique_max:
+                clique_max.append(item)
+
+        last = []
+        for C in clique_max:
+            b = _path_consistency(it - 1, B, A_0, A)
+            last.append(_get_last(C, b))
+
+        m = []
+        for item in last:
+            m.append(min(item.values()))
+            b[1][n] = min(item.values())
+        # b[1][n] = min(m)
+        print('b[1][n]: %s' % b[1][n])  # C_max lb: 382
+
+        # earliest starting time
+        b_1_j, b_i_1 = {}, {}
+        for j in V:
+            b_1_j['%s' % j] = b[1][j]
+        # print('ES: %s' % )
+        ES = b_1_j
+
+        # latest starting time
+        for i in V:
+            b_i_1['%s' % i] = -b[i][1]
+        # print('LS: %s' % )
+        LS = b_i_1
+        LB_2 = b[1][n]
+        return ES, LS, LB_2
+
+    n, T, K, p, R, r, E, V, A = get_constants(data)
+    # INIT
     A_0 = A  # A: 48
     C = []
     B = _initial_B(A_0, T)
     D = _get_F(2, A_0)
-    # 1. path consistency -> 2. immediate selection -> 'YES': update matrix B
-    # -> go to 1 - 'NO': go to 3
-    b = _path_consistency(1, B, A_0, A)
-    A = _immediate_selection(D, b, A_0)
+    b = _path_consistency(1, B, A_0, A)  # 940 updates
+    A, update = _immediate_selection(D, b, A_0)  # yes# A: 50
     B = _update_B(2, B, A_0, A)
 
-    b = _path_consistency(2, B, A_0, A)
-    A = _immediate_selection(D, b, A)
-    B = _update_B(3, B, A_0, A)
-
-    b = _path_consistency(3, B, A_0, A)
-    A = _immediate_selection(D, b, A)
-    B = _update_B(4, B, A_0, A)
-
-    # 1c. - 2c.
-    b = _path_consistency(4, B, A_0, A)
-    A = _immediate_selection(D, b, A)
-
-    # 3. symmetric triples -> 'YES': go to 2 - ' NO': go to 4
-    D = _symmetric_triples(A, b, D)  # no# D: 38
-
-    # 4. edge-finding
-    cliques = list(itertools.chain.from_iterable(_get_clique(D)))
-    cliques_max = [item for item in cliques if len(
-        item) == max(len(item) for item in cliques)]
-    clique_max = []
-    for item in cliques_max:
-        item = [int(str(item[i])) for i in range(len(item))]
-        item.sort()
-        if item not in clique_max:
-            clique_max.append(item)
-
-    last = []
-    for C in clique_max:
-        b = _path_consistency(4, B, A_0, A)
-        last.append(_get_last(C, b))
-    m = []
-    for item in last:
-        m.append(min(item.values()))
-        b[1][n] = min(item.values())
-    b[1][n] = min(m)
-    # print('b[1][n]: %s' % min(m))  # C_max lb: 38
-
-    # earliest starting time
-    b_1_j, b_i_1 = {}, {}
-    for j in V:
-        b_1_j['%s' % j] = b[1][j]
-    # print('ES: %s' % )
-    ES = b_1_j
-
-    # latest starting time
-    for i in V:
-        b_i_1['%s' % i] = -b[i][1]
-    # print('LS: %s' % )
-    LS = b_i_1
-    return n, T, K, p, R, r, E, V, A, LB, ES, LS
+    # Iteration 2 onwards apply local constraint programming alg
+    it = 2
+    while True:
+        b = _path_consistency(it, B, A_0, A)
+        A, update = _immediate_selection(D, b, A)
+        if update:
+            B = _update_B(it + 1, B, A_0, A)
+        else:
+            D, update = _symmetric_triples(A, b, D)
+            if not update:
+                break
+        it += 1
+    ES, LS, LB_2 = _edge_finding()
+    return n, T, K, p, R, r, E, V, A, ES, LS, LB_2
 
 
-def process(it=1):
-    data = load_data(it)
-    n, T, K, p, R, r, E, V, A, LB, ES, LS = algorithm(data)
-    return n, T, K, p, R, r, E, V, A, LB, ES, LS
+def process(i=1, j=1):
+    data = load_data(i, j)
+    n, T, K, p, R, r, E, V, A, ES, LS, LB_2 = algorithm(data)
+    return n, T, K, p, R, r, E, V, A, ES, LS, LB_2
 
 
 def main():
